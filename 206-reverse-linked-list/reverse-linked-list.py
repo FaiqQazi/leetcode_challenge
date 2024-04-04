70a8cf6a8473ae75ec594bc96095c22e252c2573
Faiq Qazi <112152135+FaiqQazi@users.noreply.github.com>
2024-04-04 19:02:39 +0500

    Time: 40 ms (38.55%) | Memory: 17.7 MB (33.93%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -18,18 +18,3 @@ class Solution:
             next=next.next
         current.next=prev
         return current
-# class Solution:
-#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-#         current = head
-#         prev = None
-#         if head:
-#             next_node = head.next
-#         else:
-#             return None  # Return None if head is None
-#         while current.next:
-#             current.next = prev
-#             prev = current
-#             current = next_node
-#             next_node = current.next
-#         current.next = prev  # Update the last node's next pointer
-#         return current  # Return the new head
