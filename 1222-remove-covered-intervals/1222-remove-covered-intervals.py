eb69f2c7d2ebfa929367b26ab68c4a1d545d09c8
Faiq Qazi <112152135+FaiqQazi@users.noreply.github.com>
2025-08-04 01:09:38 +0500

    Time: 107 ms (5.06%), Space: 18.4 MB (27.57%) - LeetHub

---
+++ 1222-remove-covered-intervals/1222-remove-covered-intervals.py
@@ -0,0 +1,46 @@
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        # intervals.sort(key=lambda x: (x[0], -x[1]))
+        # visited=[]
+        # # print("the intervals after the sorting are")
+        # # print(intervals)
+        # for i in range(len(intervals)-1):
+        #     # print("now checking for")
+        #     # print(intervals[i])
+        #     for j in range(i,len(intervals)):
+        #         # print(f"checking {intervals[j]} with in the {intervals[i]}")
+        #         if intervals[j][1]<=intervals[i][1] and not intervals[j] in visited and not (intervals[j][0]==intervals[i][0] and intervals[j][1]==intervals[i][1]):
+        #             # print(f" {intervals[j]} is inside {intervals[i]}")
+        #             visited.append(intervals[j])
+        #             # print("after removing the intervals is")
+        #             # print(intervals)
+        # answer=[]
+        # for n in intervals:
+        #     if n in visited:
+        #         continue
+        #     answer.append(n)
+        # return len(answer)
+
+
+        intervals.sort(key=lambda x: (x[0], -x[1]))
+        print("intervals are")
+        print(intervals)
+        visited=[]
+        prev=intervals[0]
+        for i in range(1,len(intervals)):
+            print(f"now doing for {intervals[i]}")
+            if intervals[i][1]<=prev[1]:
+                print(f"{intervals[i]} is with in {prev}")
+                visited.append(intervals[i]) 
+            else:
+                prev=intervals[i]         
+                print(f"prev is now {prev}")
+        
+        answer=[]
+        for n in intervals:
+            if n in visited:
+                continue
+            answer.append(n)
+        return len(answer)
+
+        
